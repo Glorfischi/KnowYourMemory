96c0262fa1e85c386f3ba47443ecca4fd02c5d6b
Glorfischi <Glorfischi@users.noreply.github.com>
2020-08-14 15:50:09 +0200

    Tested write_atomic for multiple senders

--- tests/write_atomic/main.cpp
+++ tests/write_atomic/main.cpp
@@ -37,94 +37,137 @@ cxxopts::ParseResult parse(int argc, char* argv[]) {
   }
 }
 
-int main(int argc, char* argv[]) {
-  auto flags = parse(argc,argv);
-  std::string ip = flags["address"].as<std::string>();  
-  bool server = flags["server"].as<bool>();  
-  int count = flags["count"].as<int>();  
-
-  std::vector<float> latency_m;
-
-  std::cout << "#### Testing WriteDuplex Latency ####" << std::endl;
-  if (server) {
-    auto ln_s = kym::connection::ListenWriteAtomic(ip, 9999);
-    if (!ln_s.ok()){
-      std::cerr << "Error listening " << ln_s.status().message() << std::endl;
-      return 1;
-    }
-    auto ln = ln_s.value();
-    auto conn_s = ln->Accept();
-    if (!conn_s.ok()){
-      std::cerr << "Error accepting " << conn_s.message() << std::endl;
-      return 1;
-    }
-
-    for(int i = 0; i<count; i++){
-      auto start = std::chrono::high_resolution_clock::now();
-      auto buf_s = ln->Receive();
-      if (!buf_s.ok()){
-        std::cerr << "Error receiving buffer " << buf_s.status().message() << std::endl;
-        ln->Close();
-        return 1;
-      }
-      auto buf = buf_s.value();
-      std::cout << *(uint64_t *)buf.addr << std::endl;
-      auto free_s = ln->Free(buf_s.value());
-      if (!free_s.ok()){
-        std::cerr << free_s << std::endl;
-        return 1;
-      }
-      auto finish = std::chrono::high_resolution_clock::now();
-      latency_m.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(finish-start).count()/1000.0);
-    }
-    ln->Close();
-  } else {
-    auto conn_s = kym::connection::DialWriteAtomic(ip, 9999);
-    if (!conn_s.ok()){
-      std::cerr << "Error dialing connection" << conn_s.status().message() << std::endl;
-      return 1;
-    }
-    auto conn = conn_s.value();
+void write_atomic_test_send(std::unique_ptr<kym::connection::WriteAtomicSender> conn, int count, int thread){
+    std::vector<float> latency_m;
 
     auto buf_s = conn->GetMemoryRegion(2*1024);
     if (!buf_s.ok()){
       std::cerr << "Error allocating send region " << buf_s.status().message() << std::endl;
       conn->Close();
-      return 1;
+      return;
     }
     auto buf = buf_s.value();
-    std::chrono::milliseconds timespan(1000);
-    std::this_thread::sleep_for(timespan); // Wait for connection to be ready
-    
     *(uint64_t *)buf.addr = 0;
+
     for(int i = 0; i<count; i++){
       *(uint64_t *)buf.addr = i;
-      std::cout << "Sending " << i << std::endl;
+      //std::cout << "Sending " << thread << " " << i << std::endl;
       auto start = std::chrono::high_resolution_clock::now();
       auto send_s = conn->Send(buf);
       if (!send_s.ok()){
         std::cerr << "Error sending buffer " << send_s.message() << std::endl;
         conn->Free(buf);
         conn->Close();
-        return 1;
+        return;
       }
       auto finish = std::chrono::high_resolution_clock::now();
       latency_m.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(finish-start).count()/1000.0);
     }
 
-    std::this_thread::sleep_for(timespan); // Wait for all acks.
+    std::chrono::milliseconds timespan(1000);
+    std::this_thread::sleep_for((1+thread)*timespan); // Poor mans mutex
     conn->Free(buf);
     conn->Close();
 
+    auto n = count;
+    std::sort (latency_m.begin(), latency_m.end());
+    int q025 = (int)(n*0.025);
+    int q500 = (int)(n*0.5);
+    int q975 = (int)(n*0.975);
+    std::cout << "## Thread " << thread << std::endl;
+    std::cout << "q025" << "\t" << "q50" << "\t" << "q975" << std::endl;
+    std::cout << latency_m[q025] << "\t" << latency_m[q500] << "\t" << latency_m[q975] << std::endl;
+}
 
-  } 
+
+void write_atomic_test_recv(std::unique_ptr<kym::connection::WriteAtomicListener> ln, int count, int threads){
+  std::vector<float> latency_m;
+
+  for (int i = 0; i<threads; i++){
+    auto conn_s = ln->Accept();
+    if (!conn_s.ok()){
+      std::cerr << "Error accepting " << i << " " << conn_s.message() << std::endl;
+      return;
+    }
+  }
+
+  for(int i = 0; i<count*threads; i++){
+    auto start = std::chrono::high_resolution_clock::now();
+    auto buf_s = ln->Receive();
+    if (!buf_s.ok()){
+      std::cerr << "Error receiving buffer " << buf_s.status().message() << std::endl;
+      ln->Close();
+      return;
+    }
+    auto buf = buf_s.value();
+    // std::cout << *(uint64_t *)buf.addr << std::endl;
+    auto free_s = ln->Free(buf_s.value());
+    if (!free_s.ok()){
+      std::cerr << free_s << std::endl;
+      return;
+    }
+    auto finish = std::chrono::high_resolution_clock::now();
+    latency_m.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(finish-start).count()/1000.0);
+  }
+  ln->Close();
+  std::chrono::milliseconds timespan(1000);
+  //std::this_thread::sleep_for((1+threads)*timespan); // Poor mans mutex
   auto n = count;
   std::sort (latency_m.begin(), latency_m.end());
   int q025 = (int)(n*0.025);
   int q500 = (int)(n*0.5);
   int q975 = (int)(n*0.975);
+  std::cout << "## Server " << std::endl;
   std::cout << "q025" << "\t" << "q50" << "\t" << "q975" << std::endl;
   std::cout << latency_m[q025] << "\t" << latency_m[q500] << "\t" << latency_m[q975] << std::endl;
+
+}
+
+
+int main(int argc, char* argv[]) {
+  auto flags = parse(argc,argv);
+  std::string ip = flags["address"].as<std::string>();  
+  int count = flags["count"].as<int>();  
+
+
+  std::cout << "#### Testing WriteDuplex Latency ####" << std::endl;
+  auto ln_s = kym::connection::ListenWriteAtomic(ip, 9999);
+  if (!ln_s.ok()){
+    std::cerr << "Error listening " << ln_s.status().message() << std::endl;
+    return 1;
+  }
+  auto ln = ln_s.value();
+
+  std::thread server_thread(write_atomic_test_recv, std::move(ln), count, 7);
+  std::chrono::milliseconds timespan(1000);
+  std::this_thread::sleep_for(timespan);
+ 
+  std::unique_ptr<kym::connection::WriteAtomicSender> snds[7];
+  std::thread snd_threads[7];
+  for (int i = 0; i < 7; i++){
+    auto conn_s = kym::connection::DialWriteAtomic(ip, 9999);
+    if (!conn_s.ok()){
+      std::cerr << "Error dialing connection" << conn_s.status().message() << std::endl;
+      return 1;
+    }
+    snds[i] = conn_s.value();
+  }
+
+  for (int i = 0; i < 7; i++){
+    snd_threads[i] = std::thread(write_atomic_test_send, std::move(snds[i]), count, i);
+  }
+  for (int i = 0; i < 7; i++){
+    snd_threads[i].join();
+  }
+  server_thread.join();
+  
+  /*auto n = count;
+  std::sort (latency_m.begin(), latency_m.end());
+  int q025 = (int)(n*0.025);
+  int q500 = (int)(n*0.5);
+  int q975 = (int)(n*0.975);
+  std::cout << "q025" << "\t" << "q50" << "\t" << "q975" << std::endl;
+  std::cout << latency_m[q025] << "\t" << latency_m[q500] << "\t" << latency_m[q975] << std::endl;*/
   
   return 0;
 }
