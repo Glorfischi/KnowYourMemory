d3e67b24992c8df4962c951d629abff9f275b938
Glorfischi <Glorfischi@users.noreply.github.com>
2020-09-03 17:20:18 +0200

    Can copy to and from DM

--- tests/micro_bench/device_memory/local.cpp
+++ tests/micro_bench/device_memory/local.cpp
@@ -33,14 +33,34 @@ int main(int argc, char* argv[]) {
 
   struct ibv_context *ctx = ln->GetContext();
   struct ibv_alloc_dm_attr attr = {};
-  attr.length = 64;
+  attr.length = 128;
   auto dm = ibv_alloc_dm(ctx, &attr);
   if (dm == nullptr) {
     std::cerr << "Error registering memory " << errno << std::endl;
     perror("error");
     return 1;
   }
-  int ret = ibv_free_dm(dm);
+
+  std::cout << "#### Copying to and from DM ####" << std::endl;
+  char src[64] = "Foobar";
+  char dst[64] = "Failed";
+
+  std::cout << "     want " << src << std::endl;
+  int ret = ibv_memcpy_to_dm(dm, 0, &src, 16);
+  if (ret != 0) {
+    std::cerr << "Error copying to DM " << ret << std::endl;
+    perror("error");
+    return 1;
+  }
+  ret = dm->memcpy_from_dm(&dst, dm, 0, 16);
+  if (ret != 0) {
+    std::cerr << "Error copying from DM " << ret << std::endl;
+    perror("error");
+    return 1;
+  }
+  std::cout << "     got  " << dst << std::endl;
+
+  ret = ibv_free_dm(dm);
   if (ret != 0) {
     std::cerr << "Error freeing memory " << ret << std::endl;
     perror("error");
