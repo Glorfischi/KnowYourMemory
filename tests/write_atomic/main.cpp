5077840082f10dd28a01f84a0c3e9243f5ec0c33
Glorfischi <Glorfischi@users.noreply.github.com>
2020-12-21 10:37:29 +0100

    Added N:1 for AW

--- tests/write_atomic/main.cpp
+++ tests/write_atomic/main.cpp
@@ -33,6 +33,7 @@ cxxopts::ParseResult parse(int argc, char* argv[]) {
       ("pingpong", "Whether to test pingpong latency", cxxopts::value<bool>())
       ("client", "Whether to as client only", cxxopts::value<bool>())
       ("server", "Whether to as server only", cxxopts::value<bool>())
+      ("c,conn",  "Number of connections" , cxxopts::value<int>()->default_value("1"))
       ("i,address", "IP address to connect to", cxxopts::value<std::string>())
       ("source", "source IP address", cxxopts::value<std::string>()->default_value(""))
       ("n,iters",  "Number of exchanges" , cxxopts::value<int>()->default_value("1000"))
@@ -83,15 +84,17 @@ int main(int argc, char* argv[]) {
   int size = flags["size"].as<int>();  
   int unack = flags["unack"].as<int>();  
 
+  int conn_count = flags["conn"].as<int>();  
 
   bool server = flags["server"].as<bool>();  
   bool client = flags["client"].as<bool>();  
 
-  std::vector<float> measurements;
+  std::vector<std::vector<float>*> measurements(conn_count);
   
   kym::connection::WriteAtomicInstance *inst = new kym::connection::WriteAtomicInstance();
 
   if (server){
+    kym::connection::WriteAtomicConnection *conns[conn_count];
     auto stat = inst->Listen(ip, 9999);
     if (!stat.ok()){
       std::cerr << "Error listening " << stat;
@@ -100,22 +103,25 @@ int main(int argc, char* argv[]) {
       exit(1);
     }
     debug(stderr, "listening\n");
-    auto conn_s = inst->Accept();
-    if (!conn_s.ok()){
-      std::cerr << "Error Accepting " << conn_s.status() << std::endl;
-      inst->Close();
-      delete inst;
-      exit(1);
+    for (int i = 0; i<conn_count; i++){
+      auto conn_s = inst->Accept();
+      if (!conn_s.ok()){
+        std::cerr << "Error Accepting " << conn_s.status() << std::endl;
+        inst->Close();
+        delete inst;
+        exit(1);
+      }
+      auto conn = conn_s.value();
+      conns[i] = conn;
+      debug(stderr, "accepted\n");
     }
-    auto conn = conn_s.value();
-    debug(stderr, "accepted\n");
-    
+      
     if (lat) {
       stat = test_lat_recv(inst, count);
     } else if (pingpong) {
-      stat = test_lat_pong(conn, inst, count, size);
+      stat = test_lat_pong(conns[0], inst, count, size);
     } else if (bw) {
-      auto bw_s = test_bw_recv(inst, count, size);
+      auto bw_s = test_bw_recv(inst, conn_count*count, size);
       if (!bw_s.ok()){
         std::cerr << "Error running bench " << bw_s.status() << std::endl;
       }
@@ -125,66 +131,74 @@ int main(int argc, char* argv[]) {
     if (!stat.ok()){
       std::cerr << "Error sending " << stat << std::endl;
     }
-    conn->Close();
-    delete conn;
   }
 
   if(client){
-    std::chrono::milliseconds timespan(1000); // If we start the server at the same time we will wait a little
+    std::vector<std::thread> workers;
+    std::chrono::milliseconds timespan(4000); // If we start the server at the same time we will wait a little
     std::this_thread::sleep_for(timespan);
-    auto conn_s = inst->Dial(ip, 9999);
-    if (!conn_s.ok()){
-      std::cerr << "Error Dialing " << conn_s.status() << std::endl;
-      inst->Close();
-      delete inst;
-      return 1;
-    }
-    
-    auto conn = conn_s.value();
-    kym::Status stat;
-    if (lat) {
-      stat = test_lat_send(conn, count, size, &measurements);
-    } else if (pingpong) {
-      stat = test_lat_ping(conn, inst, count, size, &measurements);
-    } else if (bw) {
-      auto bw_s = test_bw_send(conn, count, size, unack);
-      if (!bw_s.ok()){
-        std::cerr << "Error running bench " << bw_s.status() << std::endl;
+    kym::connection::WriteAtomicConnection *conns[conn_count];
+    for (int i = 0; i<conn_count; i++){
+      auto conn_s = inst->Dial(ip, 9999);
+      if (!conn_s.ok()){
+        std::cerr << "Error Dialing " << conn_s.status() << std::endl;
+        inst->Close();
+        delete inst;
+        return 1;
       }
-      std::cerr << "## Bandwidth (MB/s)" << std::endl;
-      std::cout << (double)bw_s.value()/(1024*1024) << std::endl;
-    }
+      
+      auto conn = conn_s.value();
+      conns[i] = conn;
+      workers.push_back(std::thread([i, bw, lat, pingpong, conn, inst, count,  size, unack, &measurements](){
+        set_core_affinity(i+2);
+        
+        std::chrono::milliseconds timespan(1000); // This is because of a race condition...
+        std::vector<float> *m = new std::vector<float>();
+        std::this_thread::sleep_for(timespan);
 
-    if (!stat.ok()){
-      std::cerr << "Error sending " << stat << std::endl;
+        kym::Status stat;
+        if (lat) {
+          stat = test_lat_send(conn, count, size, m);
+        } else if (pingpong) {
+          stat = test_lat_ping(conn, inst, count, size, m);
+        } else if (bw) {
+          auto bw_s = test_bw_send(conn, count, size, unack);
+          if (!bw_s.ok()){
+            std::cerr << "Error running bench " << bw_s.status() << std::endl;
+          }
+          m->push_back(bw_s.value());
+        }
+
+        if (!stat.ok()){
+          std::cerr << "Error sending " << stat << std::endl;
+        }
+        measurements[i] = m;
+        conn->Close();
+        delete conn;
+      }));
     }
-    conn->Close();
-    delete conn;
+    for (int i = 0; i<workers.size(); i++){
+      workers[i].join();
+    }
   }
-
-  if (!filename.empty()){
-    std::ofstream file(filename);
-    for (float f : measurements){
-      file << f << "\n";
-    }
-    file.close();
-  }
-
-
   inst->Close();
   delete inst;
-  auto n = measurements.size();
-  if (n == 0){
-    return 0;
+  if (bw) {
+    uint64_t bandwidth = 0;
+    for (auto m : measurements){
+      if (m != nullptr) {
+        bandwidth += m->at(0);
+      }
+    }
+    std::cerr << "## Bandwidth (MB/s)" << std::endl;
+    std::cout << (double)bandwidth/(1024*1024) << std::endl;
+    if (!filename.empty()){
+      std::ofstream file(filename, std::ios_base::app);
+      file << conn_count <<  " " << size << " " << unack << " "  <<  bandwidth << "\n";
+      file.close();
+    }
   }
-  std::sort (measurements.begin(), measurements.end());
-  int q025 = (int)(n*0.025);
-  int q500 = (int)(n*0.5);
-  int q975 = (int)(n*0.975);
-  std::cout << "q025" << "\t" << "q50" << "\t" << "q975" << std::endl;
-  std::cout << measurements[q025] << "\t" << measurements[q500] << "\t" << measurements[q975] << std::endl;
-
-
+  
   return 0;
 }
  
